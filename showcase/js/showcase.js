b58f8c66eed0e52bb19ea0a8f19fe511b283bd6e
Dan Leech <dan@danleech.com>
2012-12-12 17:46:31 +0000

    .

--- showcase/js/showcase.js
+++ showcase/js/showcase.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
-	$('body').css('background-color','#f00');
+	var previewHeight = $(window).height() - 60;
+	var iconPreviewHeight = previewHeight + 'px';
+	var image = "https://github.com/danleech/simple-icons/raw/master/icons/simpleicons/simpleicons-2048.png";
+	var backgroundImage = 'url(' + image + ')';
+	var colour = '#BF1813';
+	$('#preview').html('<img class="icon-preview" src=' + image + '>');
+	$('.icon-preview').css('height', previewHeight);
+	$('body').css('background', colour);
+	
+	$(window).resize(function() {
+		var previewHeight = $(window).height() - 60;
+		var iconPreviewHeight = previewHeight + 'px';
+	$('.icon-preview').css('height', previewHeight);
+	});
 });
